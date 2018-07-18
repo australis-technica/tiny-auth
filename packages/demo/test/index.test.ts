2922e8106aefaf2085aa5472ab82b909cdd8ae43
me <danieliozzi@gmail.com>
2018-07-18 10:29:35 +0200

    demo: generic test mock

--- packages/demo/test/index.test.ts
+++ packages/demo/test/index.test.ts
@@ -1,4 +1,6 @@
-describe("esecure-admin-backend", () => {
+import { join } from "path";
+
+describe(require(join(__dirname, "../package.json")).name, () => {
     it("works", () => {
         // ...
     })
