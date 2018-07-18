de2fa9f8423fac0ab5b264a009c98843800e9858
me <danieliozzi@gmail.com>
2018-07-18 12:16:03 +0200

    connect-to-server: test

--- packages/sql-connect-to-server/test/index.test.ts
+++ packages/sql-connect-to-server/test/index.test.ts
@@ -1,7 +1,23 @@
+import "@australis/load-env";
+import { execSql } from "@australis/tiny-sql";
+import connectToServer from "@australis/sql-connect-to-server";
 import { join } from "path";
-const pkg = require(join(__dirname, "../package.json"))
-describe(pkg.name, ()=> {
-    it("works", ()=> {
-        // ...
+import { Connection } from "tedious";
+const config = require(join(__dirname, "../.secrets/connection-string.json"));
+describe((require(join(__dirname, "../package.json"))).name, () => {
+    it("works", async () => {
+        let connection: Connection;
+        try {
+            connection = await connectToServer();
+            const r = await execSql(connection)<{ name }>("select name from sys.databases");
+            if (r.error) {
+                throw r.error;
+            }
+            const names = r.values.map(x => x.name).join(",");
+            expect(names.indexOf("master") !== -1).toBe(true);
+
+        } finally {
+            connection && connection.close();
+        }
     })
 });
