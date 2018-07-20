bc250bcf9a387f7ad95cb6ac5647588a57726d31
d10221 <danieliozzi@gmail.com>
2018-07-20 17:04:37 +0200

    tokenblacklist add exp as date

--- packages/token-blacklist-sql/src/init.ts
+++ packages/token-blacklist-sql/src/init.ts
@@ -10,12 +10,12 @@ export default function init(tableName: string) {
     return async (connection: Connection) => {
         const r = await ExecSql(connection)(`
     if(not(exists(select * from sys.tables where name = '${tableName}' and type = 'U')))
-    create table token_blacklist
+    create table ${tableName}
     (
         id VARCHAR(1024) NOT NULL UNIQUE,
         [iat] DATETIME NOT NULL,
-        /* tiome in seconds to expiration */
-        [exp] int NOT NULL,
+        /* time in seconds to expiration */
+        [exp] DATETIME NOT NULL,
         token varchar(max) NOT NULL
     )`);
         if (r.error) { return Promise.reject(r.error) };
