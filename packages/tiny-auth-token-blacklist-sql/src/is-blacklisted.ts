8011c803e61c98c64c7d15c69d5861a759b10306
me <danieliozzi@gmail.com>
2018-07-23 11:06:34 +0200

    token-blacklist-sql: sql query not good

--- packages/tiny-auth-token-blacklist-sql/src/is-blacklisted.ts
+++ packages/tiny-auth-token-blacklist-sql/src/is-blacklisted.ts
@@ -12,12 +12,31 @@ export default function isBlackListed(tableName: string, decode: DecodeToken) {
     return async (connection: Connection, token: string): Promise<boolean> => {
         try {
             const { token_id } = await decode(token);
-            const r = await ExecSql(connection)<{ token: string }>(`
+            let query;
+            let params;
+            if (token_id) {
+                query = `
                 select top 1 * from ${tableName} where 
                 id = @token_id OR token = @token
-                `, { token_id, token });
-            if (r.error) return Promise.reject(r.error);            
-            return r.values && r.values.length>0 &&  typeof r.values[0].token === "string" && r.values[0].token.trim() !== "";
+                `;
+                params = {
+                    token_id,
+                    token
+                }
+            } else {
+                query = `
+                select top 1 * from ${tableName} where token = @token
+                `;
+                params = {
+                    token
+                }
+            }
+            const r = await ExecSql(connection)<{ token: string }>(query, params);
+            if (r.error) {
+                return Promise.reject(r.error);
+            }
+            const ok = r.values && r.values.length > 0 && typeof r.values[0].token === "string" && r.values[0].token.trim() !== "";
+            return Promise.resolve(ok);
         } catch (error) {
             debug(error);
             return Promise.reject(error);
