11bec5962e9102ecf92e936e376bba848ae65738
d10221 <danieliozzi@gmail.com>
2018-07-24 18:02:19 +0200

    ui: viewIndex from store

--- local/tiny-auth-demo-ui/src/app-home.tsx
+++ local/tiny-auth-demo-ui/src/app-home.tsx
@@ -4,55 +4,77 @@ import { Tabs, Tab } from "@material-ui/core";
 import { CustomersView } from "./customers";
 import { ProductsView } from "./products";
 import { LicensesView } from "./licenses";
+import { RootState } from "./root";
+import { connect } from "react-redux";
+import { selectors, actions } from "./root";
+import { Dispatch } from "redux";
 /**
- * 
+ *
  */
-export interface HomeProps {
-
-}
+export interface HomeProps extends RootState {}
 /**
- * 
+ *
  */
-export default class Home extends Component<HomeProps> {
-    state = {
-        tabIndex: 0
-    }
-    setTabIndex = (tabIndex: number) => {
-        return () => {
-            this.setState({ tabIndex });
-        }
-    }
+export default connect(selectors.rawState)(
+  /** */
+  class Home extends Component<HomeProps & { dispatch: Dispatch }> {
+    set = (state: Partial<RootState>) => {
+      this.props.dispatch(actions.setState(state));
+    };
+    setTabIndex = (viewIndex: number) => {
+      return () => {
+        this.set({ viewIndex });
+      };
+    };
     /**
-     * 
+     *
      */
     renderTab = (tabIndex: number) => {
-        switch (tabIndex) {
-            case 0: {
-                return <CustomersView />
-            }
-            case 1: {
-                return <ProductsView />
-            }
-            case 2: {
-                return <LicensesView />
-            }
-            default: {
-                return null;
-            }
+      switch (tabIndex) {
+        case 0: {
+          return <CustomersView />;
         }
-    }
+        case 1: {
+          return <ProductsView />;
+        }
+        case 2: {
+          return <LicensesView />;
+        }
+        default: {
+          return null;
+        }
+      }
+    };
     /**
-     * 
+     *
      */
     render() {
-        const { tabIndex } = this.state;
-        return <React.Fragment>
-            <Tabs title="Tabs" value={tabIndex} fullWidth>
-                <Tab tabIndex={0} title="Customers" label="Customers" onClick={this.setTabIndex(0)} />
-                <Tab tabIndex={1} title="Products" label="Products" onClick={this.setTabIndex(1)} />
-                <Tab tabIndex={2} title="Licenses" label="Licenses" onClick={this.setTabIndex(2)} />
-            </Tabs>
-            {this.renderTab(tabIndex)}
+      const { viewIndex } = this.props;
+      return (
+        <React.Fragment>
+          <Tabs title="Tabs" value={viewIndex} fullWidth>
+            <Tab
+              tabIndex={0}
+              title="Customers"
+              label="Customers"
+              onClick={this.setTabIndex(0)}
+            />
+            <Tab
+              tabIndex={1}
+              title="Products"
+              label="Products"
+              onClick={this.setTabIndex(1)}
+            />
+            <Tab
+              tabIndex={2}
+              title="Licenses"
+              label="Licenses"
+              onClick={this.setTabIndex(2)}
+            />
+          </Tabs>
+          {this.renderTab(viewIndex)}
         </React.Fragment>
+      );
     }
-}
+  }
+);
