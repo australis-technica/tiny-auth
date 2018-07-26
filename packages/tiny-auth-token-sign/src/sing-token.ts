2338dde9cb302cc346221ebbee6d12a808bfbbea
me <danieliozzi@gmail.com>
2018-07-26 09:01:21 +0200

    sign-token: fix export

--- packages/tiny-auth-token-sign/src/sing-token.ts
+++ packages/tiny-auth-token-sign/src/sing-token.ts
@@ -1,12 +1,13 @@
 import signSync from "./sign-sync";
 import { defaultOptions } from "./options";
+import { DefaultOptions } from "./types";
 /**
  *
  * @param extra
  */
 export default function signToken<T extends {}>(
   extra: T,
-  options = defaultOptions
+  options: DefaultOptions = defaultOptions
 ) {
   try {
     return Promise.resolve(signSync(extra, options));
