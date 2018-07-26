18ecf1c9fbab131c5bb968e93eb77e1e33a715ee
me <danieliozzi@gmail.com>
2018-07-26 14:52:05 +0200

    demo-serve: crud-controller,  excludeKeys

--- local/tiny-auth-demo/src/crud-controller/exclude-keys.ts
+++ local/tiny-auth-demo/src/crud-controller/exclude-keys.ts
@@ -3,13 +3,13 @@
  * @param data
  * @param keys
  */
-export default function excludeKeys<T extends { [key: string]: any }, TK extends keyof T & string>(  
+export default function excludeKeys<T extends { [key: string]: any }, TK extends keyof T & string>(
   ...keys: TK[]
 ) {
   return (data: T): Partial<T> => Object.keys(data)
-    .filter(key => !(key in keys))
+    .filter(key => keys.indexOf(key as TK) === -1)
     .reduce((out, next) => {
-        out[next] = data[next];
-        return out;
+      out[next] = data[next];
+      return out;
     }, {} as Partial<T>);
 }
