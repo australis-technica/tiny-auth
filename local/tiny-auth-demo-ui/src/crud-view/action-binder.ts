63f631cae8a91ada85581ccfdfe7e99b861f19f9
d10221 <danieliozzi@gmail.com>
2018-07-27 18:23:02 +0200

    pretty

--- local/tiny-auth-demo-ui/src/crud-view/action-binder.ts
+++ local/tiny-auth-demo-ui/src/crud-view/action-binder.ts
@@ -1,55 +1,58 @@
 import { Dispatch } from "redux";
-import messages, { Message, } from "../messages";
+import messages, { Message } from "../messages";
 import getErrorMessage from "./get-error-message";
 import { AddViewState } from "./types";
 import { actionBinder as confirmActionBinder } from "../confirm-action";
 import { actionBinder as menuActionBinder } from "../menu";
 /** */
 const actionBinder = (setState: (payload: Partial<AddViewState>) => any) => {
-    /** */
-    const bindActions = (dispatch: Dispatch) => {
-        const setBusy = (busy: boolean) => {
-            dispatch(setState({ busy }));
-        }
-        const delay = (n: number) => new Promise(resolve => setTimeout(resolve, n));
-        /**
-         * 
-         * @param payload 
-         */
-        const setMessage = (payload: Message) => {
-            dispatch(messages.actions.setMessage(payload));
-        }
-        const clearMessage = () => {
-            dispatch(messages.actions.clear());
-        }
-        const setError = (error?: Error | string) => {
-            setMessage({
-                message: getErrorMessage(error),
-                status: "error"
-            })
-        }
-        const setSuccess = (message: string) => {
-            setMessage({ message, status: "success" });
-        }
-        const confirmActions = confirmActionBinder(setState, "confirmAction")(dispatch);
-        const menuActions = menuActionBinder(setState, "isMenuOpen")(dispatch);
-        return {
-            setState: (payload: Partial<AddViewState>) => {
-                dispatch(setState(payload));
-            },
-            setMessage,
-            clearMessage,
-            setError,
-            setSuccess,
-            setBusy,
-            // setConfirmAction,
-            // removeActionToConfirm,
-            // handleActionToConfirm,
-            ...confirmActions,
-            ...menuActions,
-            delay,
-        };
+  /** */
+  const bindActions = (dispatch: Dispatch) => {
+    const setBusy = (busy: boolean) => {
+      dispatch(setState({ busy }));
     };
-    return bindActions;
+
+    const delay = (n: number) => new Promise(resolve => setTimeout(resolve, n));
+
+    const setMessage = (payload: Message) => {
+      dispatch(messages.actions.setMessage(payload));
+    };
+
+    const clearMessage = () => {
+      dispatch(messages.actions.clear());
+    };
+
+    const setError = (error?: Error | string) => {
+      setMessage({
+        message: getErrorMessage(error),
+        status: "error"
+      });
+    };
+
+    const setSuccess = (message: string) => {
+      setMessage({ message, status: "success" });
+    };
+
+    const confirmActions = confirmActionBinder(setState, "confirmAction")(
+      dispatch
+    );
+
+    const menuActions = menuActionBinder(setState, "isMenuOpen")(dispatch);
+
+    return {
+      setState: (payload: Partial<AddViewState>) => {
+        dispatch(setState(payload));
+      },
+      setMessage,
+      clearMessage,
+      setError,
+      setSuccess,
+      setBusy,
+      ...confirmActions,
+      ...menuActions,
+      delay
+    };
+  };
+  return bindActions;
 };
 export default actionBinder;
