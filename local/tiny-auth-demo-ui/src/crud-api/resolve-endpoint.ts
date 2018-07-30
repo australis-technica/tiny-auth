b05b354b826d9206ae99301444da0ac584bfa177
me <danieliozzi@gmail.com>
2018-07-30 10:40:03 +0200

    ui: crud-api: resolve-endpoint, optional api-base

--- local/tiny-auth-demo-ui/src/crud-api/resolve-endpoint.ts
+++ local/tiny-auth-demo-ui/src/crud-api/resolve-endpoint.ts
@@ -1,5 +1,5 @@
 export default function resolveEndPoint(storeKey: string) {
-    const apiBase = process.env.REACT_APP_API_BASE;
+    const apiBase = process.env.REACT_APP_API_BASE || "";
     const e = process.env[`REACT_APP_API_${storeKey.toUpperCase()}`] || storeKey
-    return `${apiBase}/${e}`;
+    return [ apiBase, e].join("/");
 };
