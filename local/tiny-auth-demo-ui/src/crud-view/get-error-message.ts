36edcd9f587e76072a65700b8a334cfea03c146d
me <danieliozzi@gmail.com>
2018-08-01 08:22:52 +0200

    -crud-view (unused)

--- local/tiny-auth-demo-ui/src/crud-view/get-error-message.ts
+++
@@ -1,4 +0,0 @@
-/** */
-export default function getErrorMessage(error?: string | Error | undefined): string | undefined {
-    return !error ? undefined : typeof error === "string" ? error : error.message ? error.message : error.toString();
-}
