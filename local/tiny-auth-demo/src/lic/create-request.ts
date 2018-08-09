445687c338f2481b909948418f5ce4f078aa5868
d10221 <danieliozzi@gmail.com>
2018-08-09 04:03:46 +0200

    svc: validator url

--- local/tiny-auth-demo/src/lic/create-request.ts
+++ local/tiny-auth-demo/src/lic/create-request.ts
@@ -1,35 +1,43 @@
 import crypto from "crypto";
 import { SignRequest } from "./types";
 import secret from "./secret";
-import issuer from "./issuer"
+import issuer from "./issuer";
+import validatorUrl from "./validator-url";
 /**
- * 
+ *
  */
-export default function createRequest(payload: { exp: Date, token_id: string, aud?: string, validator?: string }): SignRequest {
-    const { token_id, exp, aud, validator, } = payload;
-    const hostname = issuer();
-    const port = process.env.PORT;
-    return Object.assign({}, {
-        expiresIn: Math.floor(((new Date(exp).valueOf() - Date.now()) / 1000)),
-        /**
-         * internal identifier
-         */
-        token_id: token_id || crypto.randomBytes(8).toString("hex"),
-        /**
-         * Issuer
-         */
-        iss: hostname,
-        /**
-         * internal.name.or.namespace.name.or.fully.qualified.client.app.name?
-         */
-        aud: aud || "*",
-        /**
-         * validator: "http://localhost:5000/validate"
-         */
-        validator: validator || `https://${hostname}:${port}/validate`,
-        /**
-         * secret: enc-key 
-         */
-        secret: secret(),
-    });
+export default function createRequest(payload: {
+  exp: Date;
+  token_id: string;
+  aud?: string;
+  validator?: string;
+  iss?: string;
+}): SignRequest {
+  const { token_id, exp, aud, validator, iss } = payload;  
+  return Object.assign(
+    {},
+    {
+      expiresIn: Math.floor((new Date(exp).valueOf() - Date.now()) / 1000),
+      /**
+       * internal identifier
+       */
+      token_id: token_id || crypto.randomBytes(8).toString("hex"),
+      /**
+       * Issuer
+       */
+      iss: iss || issuer(),
+      /**
+       * internal.name.or.namespace.name.or.fully.qualified.client.app.name?
+       */
+      aud: aud || "*",
+      /**
+       * validator: "http://localhost:5000/validate"
+       */
+      validator: validator || validatorUrl(),
+      /**
+       * secret: enc-key
+       */
+      secret: secret()
+    }
+  );
 }
