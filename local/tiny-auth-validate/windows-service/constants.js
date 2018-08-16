1abf30d1e7cbfcfb1e42707f31d563c08b2f98cd
me <danieliozzi@gmail.com>
2018-08-16 12:07:53 +0200

    validate: windows-service

---
+++ local/tiny-auth-validate/windows-service/constants.js
@@ -0,0 +1,8 @@
+const SERVICE_NAME = "TinyAuth Validate";
+const SERVICE_DESCRIPTION = "TinyAuth Validate";
+const ENTRY_POINT = "app.js";
+module.exports = {
+    ENTRY_POINT,
+    SERVICE_NAME,
+    SERVICE_DESCRIPTION,
+}
