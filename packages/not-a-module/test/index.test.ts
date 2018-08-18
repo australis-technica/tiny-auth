e684e5a75590a191b1439d13f3dae3809e78e40f
d10221 <danieliozzi@gmail.com>
2018-08-18 06:32:50 +0200

    better not-a-module

--- packages/not-a-module/test/index.test.ts
+++ packages/not-a-module/test/index.test.ts
@@ -5,6 +5,6 @@ import * as src from "../src";
  */
 describe(require(join(__dirname, "../package.json")).name, ()=> {
     it("works", ()=> {
-       expect(src.default).toBe("not-a-module")
+       expect( typeof src.default).toBe("function")
     })
 });
