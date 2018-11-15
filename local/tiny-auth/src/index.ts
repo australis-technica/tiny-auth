c05c7652d9b4fc4fc62c8626b597625e1e93e636
me <danieliozzi@gmail.com>
2018-11-15 16:11:15 +0200

    fix import/export change from local/db

--- local/tiny-auth/src/index.ts
+++ local/tiny-auth/src/index.ts
@@ -2,16 +2,18 @@ import express from "express";
 import { debugModule } from "@australis/create-debug";
 const debug = debugModule(module);
 const app = express();
-(async ()=>{ 
+(async () => {
     try {
-        require("dotenv").load({ path: process.env.ENV_PATH || ".env"});
-        const { default: init } = await import("@local/db");
-        await init();
-        const {default:  configure} = await import("@local/configure");
+        require("dotenv").load({ path: process.env.ENV_PATH || ".env" });
+        if (process.env.INIT_DB && process.env.INIT_DB.toLowerCase() === "true") {
+            const { init } = await import("@local/db");
+            await init();
+        }
+        const { default: configure } = await import("@local/configure");
         const { default: start } = await import("@local/start");
         await configure()(app);
         await start()(app);
-        debug("Started");    
+        debug("Started");
     } catch (error) {
         console.error(error);
         process.exit(-1);
