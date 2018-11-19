cbfdabe901c8cc12512c23711df42b81074ca462
Daniel <danieliozzi@gmail.com>
2018-11-19 07:29:45 +0200

    shrink auth generic options to use-ui

--- local/app/src/configure.ts
+++ local/app/src/configure.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import { Express } from "express";
 import helmet from "helmet";
 import { debugModule } from "@australis/create-debug";
-import { configure as configureAuth } from "./auth";
 
 const debug = debugModule(module);
 /** */
@@ -12,55 +11,54 @@ export type Plugin = (app: Express) => any;
  * @param app
  */
 export default () => (app: Express) => {
-    /** */
-    return new Promise(async (resolve, reject) => {
-        try {
-            // ...           
-            const { default: getFeatures } = await import("./features");
-            const features = await getFeatures();
-            if (!features.length) {
-                return Promise.reject("APP_FEATURES is Required!")
-            }
+  /** */
+  return new Promise(async (resolve, reject) => {
+    try {
+      // ...
+      const { default: getFeatures } = await import("./features");
+      const features = await getFeatures();
+      if (!features.length) {
+        return Promise.reject("APP_FEATURES is Required!");
+      }
 
-            /** Middleware */
-            app.use(
-                cors({
-                    origin: process.env.CORS_ORIGIN || "*",
-                    methods: "*",
-                    credentials: true
-                })
-            );
-
-            app.use(helmet());
-
-            await configureAuth(app);
-
-            if (features.indexOf("admin") !== -1) {
-                const { default: admin } = await import("./use-admin");
-                await admin()(app);
-            }
-
-            if (features.indexOf("ui") !== -1) {
-                const { default: useUi } = await import("./use-ui");
-                const { resolve } = await import("path");
-                await useUi({
-                    uiPath: resolve(__dirname, "../../local-app-ui/build"),
-                    /* defaultDocument: "index.html" */
-                })(app);
-                debug("Feature 'admin' configured");
-            }
-           
-            // Errors
-            const { default: errorHandler } = await import("@australis/express-plain-text-error-handler");
-            app.use((error: any, _req: any, _res: any, next: any) => {
-                next(error);
-            })
-            app.use(errorHandler());
-            debug("configured");
-            return resolve();
-        } catch (error) {
-            debug(error);
-            return reject(error);
-        }
-    });
-}
+      /** Middleware */
+      app.use(
+        cors({
+          origin: process.env.CORS_ORIGIN || "*",
+          methods: "*",
+          credentials: true,
+        }),
+      );
+      app.use(helmet());
+      {
+        const { configure } = await import("./auth");
+        await configure(app);
+      }
+      {
+        const { default: admin } = await import("./use-admin");
+        await admin()(app);
+      }
+      {
+        const { default: useUi } = await import("./use-ui");
+        const { resolve } = await import("path");
+        await useUi({
+          uiPath: resolve(__dirname, "../../app-ui/build"),
+          /* defaultDocument: "index.html" */
+        })(app);
+      }
+      // Errors
+      const {
+        default: errorHandler,
+      } = await import("@australis/express-plain-text-error-handler");
+      app.use((error: any, _req: any, _res: any, next: any) => {
+        next(error);
+      });
+      app.use(errorHandler());
+      debug("configured");
+      return resolve();
+    } catch (error) {
+      debug(error);
+      return reject(error);
+    }
+  });
+};
--- local/app/src/use-ui.ts
+++ local/app/src/use-ui.ts
@@ -1,32 +1,45 @@
-import { debugModule } from '@australis/create-debug';
+import { debugModule } from "@australis/create-debug";
 import { Express, Router, static as serveStatic } from "express";
 import { existsSync } from "fs";
 import { resolve, isAbsolute } from "path";
 
 const debug = debugModule(module);
-const defaultOptions = {
-    uiPath: "",
-    defaultDocument: "index.html"
-};
 
-/** */
+const defaultOptions = {
+  baseUrl: "/",
+  uiPath: "./public",
+  defaultDocument: "index.html",
+};
+/**
+ * Call me Last!
+ */
 export default (options: Partial<typeof defaultOptions> = defaultOptions) => {
-    const { uiPath, defaultDocument } = {
-        ...defaultOptions,
-        ...(options || defaultOptions)
-    };
-    debug({
-        uiPath,
-        defaultDocument,
-        uiPathExists: existsSync(uiPath)
-    });
-    if (!uiPath) throw new Error("uiPath is required");
-    if (!defaultDocument) throw new Error("defaultDocument  is required");
-    if(!isAbsolute(uiPath)) throw new Error(`uiPath:'${uiPath}'  Should be Absolute Path`);
-    /** */
-    return async <A extends Express | Router>(app: A): Promise<A> => {
-        app.use("/", serveStatic(options.uiPath));
-        app.use("*", serveStatic(resolve(uiPath, defaultDocument)));
+  let { uiPath, defaultDocument, baseUrl } = {
+    ...defaultOptions,
+    ...(options || defaultOptions),
+  };
+  uiPath = isAbsolute(uiPath) ? uiPath : resolve(process.cwd(), uiPath);
+  const uiPathExists = existsSync(uiPath); 
+  const defaultDocumentExists = existsSync(resolve(uiPath, defaultDocument));
+  debug({
+    uiPath,
+    defaultDocument,
+    uiPathExists,
+    defaultDocumentExists
+  });  
+  const disabled = !uiPath || !uiPathExists || !baseUrl || !defaultDocumentExists;
+  if(disabled) log("Disabled!");
+  /**
+   * @returns configure
+   */
+  return async <A extends Express | Router>(app: A): Promise<A> => {
+    if (disabled) return app;
+    app.use(`${baseUrl}`, serveStatic(options.uiPath));            
+    if (!defaultDocument || !defaultDocumentExists) {        
         return app;
-    }
-}
+    }    
+    app.use("*", serveStatic(resolve(uiPath, defaultDocument)));
+    debug("configured");
+    return app;
+  };
+};
